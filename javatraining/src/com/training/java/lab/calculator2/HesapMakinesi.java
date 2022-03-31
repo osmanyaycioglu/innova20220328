72b517d6fb9052c9d1d1f0760c724cab5924966c
osman <osman@osman123.com>
2022-03-31 16:24:54 +0300

    day 4 son

--- javatraining/src/com/training/java/lab/calculator2/HesapMakinesi.java
+++ javatraining/src/com/training/java/lab/calculator2/HesapMakinesi.java
@@ -2,6 +2,7 @@ package com.training.java.lab.calculator2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HesapMakinesi {
 
@@ -19,4 +20,40 @@ public class HesapMakinesi {
         this.hesaplamalar.add(new HesaplamaObjesi("bolme",
                                                   new BolmeHesaplama()));
     }
+
+    private void menuGoster() {
+        int index = 1;
+        for (HesaplamaObjesi hesaplamaObjesiLoc : this.hesaplamalar) {
+            System.out.println(index + "-" + hesaplamaObjesiLoc.getIslemIsmi());
+            index++;
+        }
+        System.out.println(index + "-Cikis");
+    }
+
+    public void hesapMakinasiCalistir() {
+        try (Scanner scannerLoc = new Scanner(System.in)) {
+            while (true) {
+                this.menuGoster();
+                System.out.println("Seçiminiz : ");
+                int secim = scannerLoc.nextInt();
+                if (this.hesaplamalar.size() >= secim) {
+                    System.out.println("Hesaplama için ilk deger : ");
+                    double val1Loc = scannerLoc.nextDouble();
+                    System.out.println("Hesaplama için ikinci deger : ");
+                    double val2Loc = scannerLoc.nextDouble();
+                    HesaplamaObjesi hesaplamaObjesiLoc = this.hesaplamalar.get(secim - 1);
+                    IHesaplama hesaplamaLoc = hesaplamaObjesiLoc.getHesaplama();
+                    double hesaplaLoc = hesaplamaLoc.hesapla(val1Loc,
+                                                             val2Loc);
+                    System.out.println(hesaplamaObjesiLoc.getIslemIsmi() + " işleminizin sonucu : " + hesaplaLoc);
+
+                } else {
+                    break;
+                }
+            }
+        } catch (Exception eLoc) {
+            eLoc.printStackTrace();
+        }
+    }
+
 }
---
+++ javatraining/src/com/training/java/lab/calculator2/HesapRun.java
@@ -0,0 +1,10 @@
+package com.training.java.lab.calculator2;
+
+
+public class HesapRun {
+
+    public static void main(final String[] args) {
+        HesapMakinesi hesapMakinesiLoc = new HesapMakinesi();
+        hesapMakinesiLoc.hesapMakinasiCalistir();
+    }
+}
